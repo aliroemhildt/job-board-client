9c0124890369c030ce1f8b51147481842848f626
Ali Roemhildt <ali.roemhildt@slalom.com>
2022-09-08 13:25:28 -0600

    Add query for single job data

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -2,9 +2,28 @@ import { request, gql } from 'graphql-request';
 
 const GRAPHQL_URL = 'http://localhost:9000/graphql';
 
+export const getJob = async (id) => {
+  const query = gql`
+    query JobQuery($id: ID!) {
+      job(id: $id) {
+        id
+        title
+        company {
+          id
+          name
+        }
+        description
+      }
+    }
+  `;
+  const variables = { id };
+  const { job } = await request(GRAPHQL_URL, query, variables);
+  return job;
+}
+
 export const getJobs = async () => {
   const query = gql`
-    query {
+    query JobsQuery {
       jobs {
         id
         title
